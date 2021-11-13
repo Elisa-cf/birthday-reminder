d79a5a20b80608a3211fec7daae27722249b2e88
Elisa <elisacanyelles@gmail.com>
2021-11-13 10:33:12 +0100

    added h3, List comp, button onclick

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import data from './data';
-import List from '.List';
+import List from './List';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+    <main>
+    <section className="container">
+     <h3>0 birthdays today</h3>
+     <List />
+     <button onClick={()=> console.log('you clicked me')}>Clear all</button>
+    </section>
+    </main>
   );
 }
 
