496944061ddcd50ebfeda64609ee65e36c5fc682
Elisa <elisacanyelles@gmail.com>
2021-11-13 10:40:03 +0100

    added useState, set up useState and people prop as people state value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 import data from './data';
 import List from './List';
 
 function App() {
+  const[people, setPeople] = useState(data)
   return (
     <main>
     <section className="container">
      <h3>0 birthdays today</h3>
-     <List />
+     <List people={people} />
      <button onClick={()=> console.log('you clicked me')}>Clear all</button>
     </section>
     </main>
