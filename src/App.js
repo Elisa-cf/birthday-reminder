fa2cf2c542a5c842b03795c6969138a759062f3e
Elisa <elisacanyelles@gmail.com>
2021-11-13 10:24:53 +0100

    created List file. Imported data and list  in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import List from '.List';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>Hello</h1>
     </div>
   );
 }
